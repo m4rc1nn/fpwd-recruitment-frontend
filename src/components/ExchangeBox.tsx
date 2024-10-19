f69649a7b1b012f73eed1d3391aaf5e807c89609
root <mkowalczyk193@gmail.com>
2024-10-19 16:51:10 +0200

    add limit to input

--- src/components/ExchangeBox.tsx
+++ src/components/ExchangeBox.tsx
@@ -36,6 +36,8 @@ export default function ExchangeBox() {
                                     name="eurAmount"
                                     type="number"
                                     step="0.01"
+                                    min={0}
+                                    max={1000000}
                                     placeholder="Enter EUR amount"
                                     className="pl-10"
                                     required
